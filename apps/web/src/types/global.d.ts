a17078c71febee66c6487aff6392e07f05a42ca7
Gabriel de Vasconcelos <gabrielvscode.dev@gmail.com>
2026-02-26 23:54:35 -0300

    fix: recharts has no exported members

--- apps/web/src/types/global.d.ts
+++ apps/web/src/types/global.d.ts
@@ -24,6 +24,27 @@ declare module 'next-themes' {
 }
 
 declare module 'recharts' {
+  import * as React from 'react';
+  
+  // Component exports - using any to avoid strict type checking issues
+  export const BarChart: React.ComponentType<any>;
+  export const Bar: React.ComponentType<any>;
+  export const XAxis: React.ComponentType<any>;
+  export const YAxis: React.ComponentType<any>;
+  export const CartesianGrid: React.ComponentType<any>;
+  export const PieChart: React.ComponentType<any>;
+  export const Pie: React.ComponentType<any>;
+  export const Cell: React.ComponentType<any>;
+  export const Area: React.ComponentType<any>;
+  export const AreaChart: React.ComponentType<any>;
+  export const Legend: React.ComponentType<any>;
+  export const ComposedChart: React.ComponentType<any>;
+  export const LineChart: React.ComponentType<any>;
+  export const Line: React.ComponentType<any>;
+  export const Tooltip: React.ComponentType<any>;
+  export const ResponsiveContainer: React.ComponentType<any>;
+  
+  // Type interfaces
   export interface TooltipProps<ValueType, NameType> {
     active?: boolean;
     payload?: Array<{
